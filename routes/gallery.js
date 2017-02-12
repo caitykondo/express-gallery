500bd76ab93bd632b6f5a68e1f03e9039c3c95c4
Caity Kondo <caitykondo@Caitys-MacBook-Pro-2.local>
2017-02-11 14:51:21 -1000

    moved POST method to gallery/new route

--- routes/gallery.js
+++ routes/gallery.js
@@ -5,11 +5,16 @@ const db = require('./../models');
 const { Photo } = db;
 
 router.route('/')
-  .get(( req, res ) => {
+  .get((req, res) => {
     Photo.findAll()
       .then((photos) => res.send(photos));
+  });
+
+router.route('/new')
+  .get((req, res) => {
+    //
   })
-  .post(( req, res ) => {
+  .post((req, res) => {
     Photo.create({author: req.body.author, link: req.body.link, description: req.body.description })
       .then((photo) => res.send(photo));
   });
