66af7aaa0dfd8359cd69ce1807c819259330b816
Caity Kondo <caitykondo@Caitys-MacBook-Pro-2.local>
2017-02-11 15:15:35 -1000

    setting up gulp w sass

--- server.js
+++ server.js
@@ -1,7 +1,16 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const handlebars = require('express-handlebars');
 const PORT = process.env.PORT || 3000;
 const app = express();
+
+const hbs = handlebars.create({
+  extname: 'hbs',
+  defaultLayout: 'app'
+});
+
+app.engine('hbs', hbs.engine);
+app.set('view engine', 'hbs');
 
 app.use(express.static('./'));
 app.use(bodyParser.urlencoded({ extended : false }));
