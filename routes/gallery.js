5e44b7bac5acb18ffdf59385913e4dc9052f6170
Caity Kondo <caitykondo@Caitys-MacBook-Pro-2.local>
2017-02-11 16:49:06 -1000

    can render photo by id //hack hbs with each helper

--- routes/gallery.js
+++ routes/gallery.js
@@ -16,7 +16,19 @@ router.route('/new')
   })
   .post((req, res) => {
     Photo.create({author: req.body.author, link: req.body.link, description: req.body.description })
-      .then((photo) => res.send(photo));
+      .then((photo) => res.render('./gallery/photo', {photo}));
+  });
+
+router.route('/:id')
+  .get((req, res) => {
+    Photo.findAll({ where: { id: req.params.id}} )
+      .then((photos) => res.render('./gallery/photo', {photos}));
+
+      // .then((photo) => {
+      //   console.log(photo[0].description);
+      //   res.render('./gallery/photo', photo[0])
+      // }
+      // );
   });
 
 module.exports = router;
