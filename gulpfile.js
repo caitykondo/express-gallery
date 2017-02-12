f020be108737399eeaec349bb2a8306a74a32cb8
Caity Kondo <caitykondo@Caitys-MacBook-Pro-2.local>
2017-02-11 20:45:42 -1000

    styled header

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,6 @@
 const gulp = require('gulp');
 const scss = require('gulp-sass');
-const browserSync = require('browser-sync').create();
+// const browserSync = require('browser-sync').create();
 
 gulp.task('scss', () => {
   return gulp.src('./scss/*.scss')
@@ -8,16 +8,16 @@ gulp.task('scss', () => {
     .pipe(gulp.dest('./public/css'))
 })
 
-gulp.task('browserSync', () => {
-  return browserSync.init(['./public'], {
-    server: {
-      baseDir: './public'
-    }
-  })
-})
+// gulp.task('browserSync', () => {
+//   return browserSync.init(['./public'], {
+//     server: {
+//       baseDir: './public'
+//     }
+//   })
+// })
 
-gulp.task('watch', ['browserSync'], () => {
+gulp.task('watch', () => {
   gulp.watch('./scss/**/*.scss', ['scss'])
 })
 
-gulp.task('default', ['watch', 'scss'])
+gulp.task('default', ['watch'])
