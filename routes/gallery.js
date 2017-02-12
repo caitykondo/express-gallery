d5538726a78a3eb87c8e1858a6cef3d84e31f102
Caity Kondo <caitykondo@Caitys-MacBook-Pro-2.local>
2017-02-11 14:47:41 -1000

    set up GET and POST methods to interact with database gallery

--- routes/gallery.js
+++ routes/gallery.js
@@ -1,9 +1,17 @@
 const express = require('express');
 let router = express.Router();
 
+const db = require('./../models');
+const { Photo } = db;
+
 router.route('/')
-  .get((req, res)=> {
-    res.send('working');
+  .get(( req, res ) => {
+    Photo.findAll()
+      .then((photos) => res.send(photos));
+  })
+  .post(( req, res ) => {
+    Photo.create({author: req.body.author, link: req.body.link, description: req.body.description })
+      .then((photo) => res.send(photo));
   });
 
 module.exports = router;
