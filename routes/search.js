ff489cc4d5bae4db9586540bf6bba195e083ac43
Caity Kondo <caitykondo@Caitys-MacBook-Pro-2.local>
2017-03-01 22:03:14 -1000

    recfactored search code

--- routes/search.js
+++ routes/search.js
@@ -4,16 +4,33 @@ const db = require('./../models');
 const { Photo } = db;
 
 router.route('/')
+  .get((req, res)=>{
+    res.redirect(303, 'gallery');
+  })
   .post((req, res)=>{
+    res.redirect(303, `search/${req.body.keyword}`);
+});
+
+router.route('/:keyword')
+  .get((req, res) => {
     Photo.findAll({
       where: {
-        description: {
-          $like: `%${req.body.keyword}%`
+        $or: [
+          {
+            description: {
+              $like: `%${req.params.keyword}%`
+            }
+          },
+        {
+          author: {
+            $like: `%${req.params.keyword}%`
+          }
         }
-      }
-    })
-      .then((photos)=> {
-        res.render('./search', {photos});
-    });
+      ]
+    }
+    }).then((photos)=> {
+      res.render('./search', { photos });
   });
+});
+
 module.exports = router;
